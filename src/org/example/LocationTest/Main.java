08632a35a781d25d0a74d986724276e318a312e3
Josenivaldo Benito Jr <j.junior@samsung.com>
2012-02-09 08:47:35 -0800

    Correct warning, modify a little to give time information and give a statement about code origin

--- src/org/example/LocationTest/Main.java
+++ src/org/example/LocationTest/Main.java
@@ -1,3 +1,9 @@
+/* 
+ * this package is based on examples obtained in the stackflow website
+ * mainly http://stackoverflow.com/questions/938719/android-using-locationmanager-does-not-give-a-geo-fix
+ * it is supposed to serve as proof of concept only. No warranty.
+ *  
+ */
 package org.example.LocationTest;
 
 import android.app.Activity;
@@ -5,12 +11,12 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 public class Main extends Activity implements LocationListener{
     private LocationManager myManager;
     private TextView tv;
+    private long deltaT = 0, lastT = 0, greaterDeltaT = 0;
 
 
     /********************************************************************** 
@@ -25,7 +31,7 @@ public class Main extends Activity implements LocationListener{
         tv = (TextView) findViewById(R.id.TextView01);
 
         // set up the LocationManager
-        myManager = (LocationManager) getSystemService(LOCATION_SERVICE);   
+        myManager = (LocationManager) getSystemService(LOCATION_SERVICE);
     }
 
     @Override
@@ -54,7 +60,7 @@ public class Main extends Activity implements LocationListener{
     private void startListening() {
         myManager.requestLocationUpdates(
             LocationManager.GPS_PROVIDER, 
-            0, 
+            300000, 
             0, 
             this
         );
@@ -72,14 +78,31 @@ public class Main extends Activity implements LocationListener{
      * LocationListener overrides below 
      **********************************************************************/
     public void onLocationChanged(Location location) {
-        // we got new location info. lets display it in the textview
+    	long currentT = 0;
+        // we got new location info.
+    	currentT = location.getTime();
+    	
+    	if (lastT != 0) {
+    		deltaT = currentT - lastT;
+    	
+    		// Calculate greater delta T
+    		if ((deltaT > greaterDeltaT) && (deltaT != currentT)) {
+    			greaterDeltaT = deltaT;
+    		}
+    	}
+    	
+    	//  lets display it in the textview
         String s = "";
-        s += "Time: "        + location.getTime() + "\n";
+        s += "Time     : "   + currentT + "\n";
+        s += "Delta   T: "   + deltaT + "\n";
+        s += "Greater T: "   + greaterDeltaT + "\n";
+        s += "Provider: "    + location.getProvider() + "\n";
         s += "\tLatitude:  " + location.getLatitude()  + "\n";
         s += "\tLongitude: " + location.getLongitude() + "\n";
         s += "\tAccuracy:  " + location.getAccuracy()  + "\n";
-
+                
         tv.setText(s);
+        lastT = currentT;
     }    
 
     public void onProviderDisabled(String provider) {}    
